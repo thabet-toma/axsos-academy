2349787a051183356f8556c4d94797bad29baab1
thabet toma <thapet64@gmail.com>
2022-08-30 22:28:01 +0300

    like

---
+++ WEB FUNDAMENTALS/js/like/js.js
@@ -0,0 +1,16 @@
+var y=[document.querySelector(".like1"),document.querySelector(".like2"),document.querySelector(".like3")]
+var count=[9,12,9]
+function add1(){
+count[0]++
+y[0].innerText=count[0]+" like(s)";
+
+}
+function add2(){
+    count[1]++
+    y[1].innerText=count[1]+" like(s)";
+    
+    }function add3(){
+        count[2]++
+        y[2].innerText=count[2]+" like(s)";
+        
+        }
