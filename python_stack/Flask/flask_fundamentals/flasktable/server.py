87542124cfd4a66b53b845c6d2b7ead4e33fd497
thabet toma <thapet64@gmail.com>
2022-09-18 12:50:27 +0300

    flask-table

---
+++ python_stack/Flask/flask_fundamentals/flasktable/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app= Flask(__name__)
+
+@app.route("/")
+def tabledict():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+
+    return render_template("index.html", var1 = users, obj=users[0])
+
+
+
+
+
+
+
+
+
+if(__name__)=="__main__":
+    app.run(debug=True)
