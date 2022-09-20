de3c6c20fbbdd5ac10a6cccda14d1859b0400a76
thabet toma <thapet64@gmail.com>
2022-09-20 15:02:48 +0300

    time_display

---
+++ python_stack/django/TimeDisplayproj/time_display/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from time import gmtime, strftime
+
+def time(request):
+    context = {
+        "time1": strftime("%Y-%m-%d", gmtime()),
+        "time2":strftime(" %H:%M %p",gmtime())
+    }
+    return render(request,'index.html', context)
