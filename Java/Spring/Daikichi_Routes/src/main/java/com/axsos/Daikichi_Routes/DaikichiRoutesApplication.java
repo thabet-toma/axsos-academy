fdd00970d47de72e0b9e112dd4dc2d59b499c5fa
thabet toma <thapet64@gmail.com>
2022-10-30 13:50:04 +0300

    welcome

---
+++ Java/Spring/Daikichi_Routes/src/main/java/com/axsos/Daikichi_Routes/DaikichiRoutesApplication.java
@@ -0,0 +1,30 @@
+package com.axsos.Daikichi_Routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+	@RequestMapping("")
+	public String index() {
+		return "Welcome";
+	}
+	@RequestMapping("/today")
+	public String today() {
+		return"Today you will find luck in all your endeavors";
+	}
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+
+}
---
+++ Java/Spring/Daikichi_Routes/src/test/java/com/axsos/Daikichi_Routes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.Daikichi_Routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
