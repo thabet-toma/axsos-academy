9c4563291520648c98708b0b739bd9ec33388e73
thabet toma <thapet64@gmail.com>
2022-10-31 14:41:04 +0300

    pathvariable

--- Java/Spring/Daikichi_Routes/src/main/java/com/axsos/Daikichi_Routes/DaikichiRoutesApplication.java
+++ Java/Spring/Daikichi_Routes/src/main/java/com/axsos/Daikichi_Routes/DaikichiRoutesApplication.java
@@ -1,7 +1,9 @@
 package com.axsos.Daikichi_Routes;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,21 @@ public class DaikichiRoutesApplication {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	@RequestMapping("/travel/{name}")
+	public String name(@PathVariable("name") String name) {
+		return "Congratulations! You will soon travel to'."+name;
+	}
+	@RequestMapping("/lotto/{num}")
+	public String oddeven(@PathVariable("num") int num) {
+		if(num%2==0) {
+		return "You will take a grand journey in the near future, but be weary of tempting offers";
+	}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+			}
+		}
+	
+
 
 
 }
