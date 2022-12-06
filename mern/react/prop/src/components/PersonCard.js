cb9c01b56099748204c5a5cdc75b0533c5df7287
thabet toma <thapet64@gmail.com>
2022-12-06 16:17:12 +0300

    state

--- mern/react/prop/src/components/PersonCard.js
+++ mern/react/prop/src/components/PersonCard.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state={age:this.props.age}}
+
+
     render(){
     return(
          <div>
          <h1>{this.props.firstName},{this.props.lastName}</h1>
-         <p>age:{this.props.age}</p>
+         <p>age:{this.state.age}</p>
         <p>hairColor:{this.props.color}</p>
+        <button onClick={() => { this.setState({ age: this.state.age+1 }) } }>birthday button for {this.props.firstName}</button>
         </div>)
     
  }}
