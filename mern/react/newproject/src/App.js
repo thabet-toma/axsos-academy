8672a2257a1a8de4e7404b6152ec83079dc3cf17
thabet toma <thapet64@gmail.com>
2022-12-06 14:30:43 +0300

    first js6

---
+++ mern/react/newproject/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <h1 >hello Dojo</h1>
+      <h2>things i need to do</h2>
+      <ul>
+        <li>learn react</li>
+        <li>climp mt everst</li>
+        <li>run a marthon</li>
+        <li>feed the dogs</li>
+
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
